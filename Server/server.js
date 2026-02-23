3420a5703d1cee7f2b1b3ef1e052901ef41f61cf
0mairAftab <omairch035@gmail.com>
2026-02-23 22:04:12 +0500

    Connected mongodb atlas

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app=express();
+
+// SEPARATE DB FILE BNANE KI BJAE YAHAN E CONNECTION KR DIYA
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("DB Error:", err);
+  });
 
 
 
-app.listen(8000,()=>{console.log("Server running on port 8000")}) 
+  
+
+const app = express();
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
