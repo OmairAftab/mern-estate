b85bc6123eb587bd13dd2e9fe2646ad206ea46c8
0mairAftab <omairch035@gmail.com>
2026-04-05 13:23:18 +0500

    removed verifying token middleware from create route of listing

--- Server/Routes/listingRoutes.js
+++ Server/Routes/listingRoutes.js
@@ -6,7 +6,7 @@ import { verifyTheToken } from "../MiddleWare/VerificationMW.js";
 const router=express.Router();
 
 
-router.post('/create', verifyTheToken,createListing)
+router.post('/create',createListing)
 
 
  
